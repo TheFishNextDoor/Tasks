976dfb6c1eaeaeb7d0d0a0dff08cb12be4bfc49e
TheFishNextDoor <unconfigured@null.spigotmc.org>
2024-05-12 14:48:50 -0500

    added reload command

--- src/main/java/com/thefishnextdoor/tasks/command/Tasks.java
+++ src/main/java/com/thefishnextdoor/tasks/command/Tasks.java
@@ -1,5 +1,6 @@
 package com.thefishnextdoor.tasks.command;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.bukkit.command.Command;
@@ -8,6 +9,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 
+import com.thefishnextdoor.tasks.TasksPlugin;
 import com.thefishnextdoor.tasks.player.PlayerProfile;
 import com.thefishnextdoor.tasks.task.PlayerTask;
 
@@ -15,13 +17,30 @@ import net.md_5.bungee.api.ChatColor;
 
 public class Tasks implements CommandExecutor, TabCompleter {
 
+    private final String RELOAD_PERMISSION = "tasks.reload";
+
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        if (args.length == 1) {
+            ArrayList<String> subcommands = new ArrayList<String>();
+            if (sender.hasPermission(RELOAD_PERMISSION)) {
+                subcommands.add("reload");
+            }
+            return subcommands;
+        }
         return null;
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (args.length > 0) {
+            if (args[0].equalsIgnoreCase("reload") && sender.hasPermission(RELOAD_PERMISSION)) {
+                TasksPlugin.loadConfigs();
+                sender.sendMessage(ChatColor.BLUE + "Plugin reloaded");
+                return true;
+            }
+        }
+
         if (!(sender instanceof Player)) {
             sender.sendMessage(ChatColor.RED + "You must be a player to use this command.");
             return true;
