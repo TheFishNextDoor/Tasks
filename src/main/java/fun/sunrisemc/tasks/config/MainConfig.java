fa613176f9a3c43d8e8c055af21a73dc4ea60936
TheFishNextDoor <wonderfulwiz9@gmail.com>
2025-09-30 12:42:12 -0500

    refactor: replace ConfigUtils methods with direct implementations in MainConfig

--- src/main/java/fun/sunrisemc/tasks/config/MainConfig.java
+++ src/main/java/fun/sunrisemc/tasks/config/MainConfig.java
@@ -3,7 +3,6 @@ package fun.sunrisemc.tasks.config;
 import org.bukkit.configuration.file.YamlConfiguration;
 
 import fun.sunrisemc.tasks.utils.ConfigFile;
-import fun.sunrisemc.tasks.utils.ConfigUtils;
 
 public class MainConfig {
 
@@ -26,15 +25,25 @@ public class MainConfig {
         this.ENABLE_LEVELLING = config.getBoolean("levels.enable", true);
         this.SHOW_LEVEL = config.getBoolean("levels.show-level", true);
         this.CHAT_FORMAT = config.getString("levels.chat-format", "&f[{color}{level}&f] {message}");
-        this.LEVEL_BASE = ConfigUtils.getDoubleClamped(config, "levels.xp-curve.base", 1.0, 1_000_000.0);
-        this.LEVEL_MULTIPLIER = ConfigUtils.getDoubleClamped(config, "levels.xp-curve.multiplier", 1.0, 10.0);
+        this.LEVEL_BASE = getDoubleClamped(config, "levels.xp-curve.base", 1.0, 1_000_000.0);
+        this.LEVEL_MULTIPLIER = getDoubleClamped(config, "levels.xp-curve.multiplier", 1.0, 10.0);
         this.ALLOW_TASK_SKIPPING = config.getBoolean("tasks.allow-skipping", true);
-        this.MAX_TASKS = ConfigUtils.getIntClamped(config, "tasks.max-tasks", 0, 20);
-        this.TASK_XP_MULTIPLIER = ConfigUtils.getDoubleClamped(config, "tasks.reward-xp-multiplier", 0.0, 1_000_000.0);
-        this.TASK_MONEY_MULTIPLIER = ConfigUtils.getDoubleClamped(config, "tasks.reward-money-multiplier", 0.0, 1_000_000.0);
+        this.MAX_TASKS = getIntClamped(config, "tasks.max-tasks", 0, 20);
+        this.TASK_XP_MULTIPLIER = getDoubleClamped(config, "tasks.reward-xp-multiplier", 0.0, 1_000_000.0);
+        this.TASK_MONEY_MULTIPLIER = getDoubleClamped(config, "tasks.reward-money-multiplier", 0.0, 1_000_000.0);
     }
 
     private void updateConfig() {
         ConfigFile.moveKeyIfExists("config", "xp-curve", "levels.xp-curve");
+    }
+
+    private int getIntClamped(YamlConfiguration config, String path, int min, int max) {
+        int value = config.getInt(path);
+        return Math.clamp(value, min, max);
+    }
+
+    private double getDoubleClamped(YamlConfiguration config, String path, double min, double max) {
+        double value = config.getDouble(path);
+        return Math.clamp(value, min, max);
     }
 }
