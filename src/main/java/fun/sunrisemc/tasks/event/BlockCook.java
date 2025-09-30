efeaa0446094d619e7aeda2a17c51ad875805102
TheFishNextDoor <wonderfulwiz9@gmail.com>
2025-09-30 12:38:31 -0500

    refactor: move getNearestPlayer method from PlayerUtils to BlockCook class

--- src/main/java/fun/sunrisemc/tasks/event/BlockCook.java
+++ src/main/java/fun/sunrisemc/tasks/event/BlockCook.java
@@ -10,11 +10,11 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockCookEvent;
 import org.bukkit.inventory.ItemStack;
+import org.checkerframework.checker.nullness.qual.NonNull;
 
 import fun.sunrisemc.tasks.player.PlayerProfile;
 import fun.sunrisemc.tasks.player.PlayerProfileManager;
 import fun.sunrisemc.tasks.task.TriggerType;
-import fun.sunrisemc.tasks.utils.PlayerUtils;
 
 public class BlockCook implements Listener {
 
@@ -25,12 +25,25 @@ public class BlockCook implements Listener {
             return;
         }
         Location location = block.getLocation();
-        Optional<Player> player = PlayerUtils.getNearestPlayer(location);
+        Optional<Player> player = getNearestPlayer(location);
         if (!player.isPresent()) {
             return;
         }
         PlayerProfile playerProfile = PlayerProfileManager.get(player.get());
         ItemStack result = event.getResult();
         playerProfile.triggerTasks(TriggerType.SMELT_ITEM, location, player.get(), result, block, result.getAmount());
+    }
+
+    private Optional<Player> getNearestPlayer(@NonNull Location location) {
+        Player nearestPlayer = null;
+        double nearestDistance = Double.MAX_VALUE;
+        for (Player player : location.getWorld().getPlayers()) {
+            double distance = player.getLocation().distance(location);
+            if (distance < nearestDistance) {
+                nearestPlayer = player;
+                nearestDistance = distance;
+            }
+        }
+        return Optional.ofNullable(nearestPlayer);
     }
 }
--- src/main/java/fun/sunrisemc/tasks/utils/PlayerUtils.java
+++ src/main/java/fun/sunrisemc/tasks/utils/PlayerUtils.java
@@ -1,27 +1,10 @@
 package fun.sunrisemc.tasks.utils;
 
 import java.util.ArrayList;
-import java.util.Optional;
-
 import org.bukkit.Bukkit;
-import org.bukkit.Location;
 import org.bukkit.entity.Player;
-import org.checkerframework.checker.nullness.qual.NonNull;
 
 public class PlayerUtils {
-
-    public static Optional<Player> getNearestPlayer(@NonNull Location location) {
-        Player nearestPlayer = null;
-        double nearestDistance = Double.MAX_VALUE;
-        for (Player player : location.getWorld().getPlayers()) {
-            double distance = player.getLocation().distance(location);
-            if (distance < nearestDistance) {
-                nearestPlayer = player;
-                nearestDistance = distance;
-            }
-        }
-        return Optional.ofNullable(nearestPlayer);
-    }
 
     public static ArrayList<String> getPlayerNames() {
         ArrayList<String> playerNames = new ArrayList<>();
