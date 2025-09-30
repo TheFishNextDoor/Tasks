e3578e178138e0a2c584a187ce5bffb8b508b3e2
TheFishNextDoor <wonderfulwiz9@gmail.com>
2025-09-30 12:56:33 -0500

    refactor: standardize constant naming conventions in Money class

--- src/main/java/fun/sunrisemc/tasks/utils/Money.java
+++ src/main/java/fun/sunrisemc/tasks/utils/Money.java
@@ -4,33 +4,32 @@ import net.md_5.bungee.api.ChatColor;
 
 public class Money {
 
-    private static final String currencyPrefix = "$";
-    private static final String currencySuffix = "";
-
-    private static final String positivePrefix = ChatColor.GOLD + "";
-    private static final String negativePrefix = ChatColor.RED + "-";
+    private static final String CURRENCY_PREFIX = "$";
+    private static final String CURRENCY_SUFFIX = "";
+    private static final String POSITIVE_PREFIX = ChatColor.GOLD + "";
+    private static final String NEGATIVE_PREFIX = ChatColor.RED + "-";
 
     public static String format(double money) {
-        String color = money < 0 ? negativePrefix : positivePrefix;
+        String color = money < 0 ? NEGATIVE_PREFIX : POSITIVE_PREFIX;
         money = Math.abs(money);
         if (money >= 1000000000) {
             double billion = money / 1000000000;
             String formattedMoney = billion % 1 == 0 ? String.format("%.0f", billion) : String.format("%.1f", billion);
-            return color + currencyPrefix + formattedMoney + "b" + currencySuffix;
+            return color + CURRENCY_PREFIX + formattedMoney + "b" + CURRENCY_SUFFIX;
         } 
         else if (money >= 1000000) {
             double million = money / 1000000;
             String formattedMoney = million % 1 == 0 ? String.format("%.0f", million) : String.format("%.1f", million);
-            return color + currencyPrefix + formattedMoney + "m" + currencySuffix;
+            return color + CURRENCY_PREFIX + formattedMoney + "m" + CURRENCY_SUFFIX;
         } 
         else if (money >= 1000) {
             double thousand = money / 1000;
             String formattedMoney = thousand % 1 == 0 ? String.format("%.0f", thousand) : String.format("%.1f", thousand);
-            return color + currencyPrefix + formattedMoney + "k" + currencySuffix;
+            return color + CURRENCY_PREFIX + formattedMoney + "k" + CURRENCY_SUFFIX;
         } 
         else {
             String formattedMoney = money % 1 == 0 ? String.format("%.0f", money) : String.format("%.2f", money);
-            return color + currencyPrefix + formattedMoney + currencySuffix;
+            return color + CURRENCY_PREFIX + formattedMoney + CURRENCY_SUFFIX;
         } 
     }
 }
