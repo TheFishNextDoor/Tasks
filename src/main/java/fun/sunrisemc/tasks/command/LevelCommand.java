d518136ae91ceb930d759d8f2f23ec249ed7603b
TheFishNextDoor <wonderfulwiz9@gmail.com>
2025-11-19 14:23:06 -0600

    update level command

--- src/main/java/fun/sunrisemc/tasks/command/LevelCommand.java
+++ src/main/java/fun/sunrisemc/tasks/command/LevelCommand.java
@@ -2,11 +2,15 @@ package fun.sunrisemc.tasks.command;
 
 import java.util.List;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import fun.sunrisemc.tasks.TasksPlugin;
 import fun.sunrisemc.tasks.config.MainConfig;
@@ -14,35 +18,47 @@ import fun.sunrisemc.tasks.player.PlayerProfile;
 import fun.sunrisemc.tasks.player.PlayerProfileManager;
 import fun.sunrisemc.tasks.unlock.Unlock;
 import fun.sunrisemc.tasks.unlock.UnlockManager;
-import net.md_5.bungee.api.ChatColor;
 
 public class LevelCommand implements CommandExecutor, TabCompleter {
 
     @Override
-    public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
+    @Nullable
+    public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String @NotNull [] args) {
         return null;
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String @NotNull [] args) {
+        // Ensure sender is a player
         if (!(sender instanceof Player)) {
             sender.sendMessage(ChatColor.RED + "You must be a player to use this command.");
             return true;
         }
+
         Player player = (Player) sender;
 
+        // Get plugin config
         MainConfig config = TasksPlugin.getMainConfig();
+
+        // Check if leveling is enabled
         if (!config.ENABLE_LEVELLING) {
             player.sendMessage(ChatColor.RED + "Leveling is disabled on this server.");
             return true;
         }
         
+        // Get player profile
         PlayerProfile playerProfile = PlayerProfileManager.get(player);
+
+        // Get player info
         int level = playerProfile.getLevel();
         int xpSinceLastLevel = playerProfile.getXpSinceLastLevel();
         int xpToNextLevel = xpSinceLastLevel + playerProfile.getXpToNextLevel();
         String color = playerProfile.getColor();
+
+        // Send level
         player.sendMessage(color + ChatColor.BOLD + "Level " + level + ChatColor.WHITE + " (" + xpSinceLastLevel + "/" + xpToNextLevel + ")");
+
+        // Send skips info
         if (TasksPlugin.getMainConfig().ALLOW_TASK_SKIPPING) {
             int skips = playerProfile.getSkips();
             if (skips == 1) {
@@ -52,6 +68,8 @@ public class LevelCommand implements CommandExecutor, TabCompleter {
                 player.sendMessage(ChatColor.WHITE + "You have " + skips + " skips remaining.");
             }
         }
+
+        // Send upcoming unlocks
         player.sendMessage(color + ChatColor.BOLD + "Upcoming Unlocks");
         int shownUnlocks = 0;
         for (Unlock unlock : UnlockManager.getSorted()) {
@@ -66,6 +84,7 @@ public class LevelCommand implements CommandExecutor, TabCompleter {
         if (shownUnlocks == 0) {
             player.sendMessage(ChatColor.WHITE + "No upcoming unlocks");
         }
+        
         return true;
     }
 }
