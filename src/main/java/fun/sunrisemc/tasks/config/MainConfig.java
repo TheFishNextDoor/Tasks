18b36a940c92fa1d56f6e1a29bb6201c86d98e9d
TheFishNextDoor <wonderfulwiz9@gmail.com>
2025-11-26 13:28:11 -0600

    add main config logging

--- src/main/java/fun/sunrisemc/tasks/config/MainConfig.java
+++ src/main/java/fun/sunrisemc/tasks/config/MainConfig.java
@@ -4,6 +4,7 @@ import org.bukkit.configuration.file.YamlConfiguration;
 
 import org.jetbrains.annotations.NotNull;
 
+import fun.sunrisemc.tasks.TasksPlugin;
 import fun.sunrisemc.tasks.file.ConfigFile;
 import fun.sunrisemc.tasks.utils.YAMLUtils;
 
@@ -25,21 +26,23 @@ public class MainConfig {
     public final double TASK_MONEY_MULTIPLIER;
 
     public MainConfig() {
+        TasksPlugin.logInfo("Loading main config...");
+
         YamlConfiguration config = ConfigFile.get("config", true);
 
         // Levels settings
-
-        this.ENABLE_LEVELLING = config.getBoolean("levels.enable", true);
-        this.SHOW_LEVEL = config.getBoolean("levels.show-level", true);
+        this.ENABLE_LEVELLING = YAMLUtils.getBoolean(config, "levels.enable").orElse(true);
+        this.SHOW_LEVEL = YAMLUtils.getBoolean(config, "levels.show-level").orElse(true);
         this.CHAT_PREFIX_FORMAT = YAMLUtils.getString(config, "levels.chat-prefix-format").orElse("&f[{color}{level}&f] ");
         this.LEVEL_BASE = YAMLUtils.getDoubleClamped(config, "levels.xp-curve.base", 1.0, 1_000_000.0).orElse(10.0);
         this.LEVEL_MULTIPLIER = YAMLUtils.getDoubleClamped(config, "levels.xp-curve.multiplier", 1.0, 10.0).orElse(1.02);
 
         // Tasks settings
-
-        this.ALLOW_TASK_SKIPPING = config.getBoolean("tasks.allow-skipping", true);
+        this.ALLOW_TASK_SKIPPING = YAMLUtils.getBoolean(config, "tasks.allow-skipping").orElse(true);
         this.MAX_TASKS = YAMLUtils.getIntClamped(config, "tasks.max-tasks", 0, 20).orElse(8);
         this.TASK_XP_MULTIPLIER = YAMLUtils.getDoubleClamped(config, "tasks.reward-xp-multiplier", 0.0, 1_000_000.0).orElse(1.0);
         this.TASK_MONEY_MULTIPLIER = YAMLUtils.getDoubleClamped(config, "tasks.reward-money-multiplier", 0.0, 1_000_000.0).orElse(1.0);
+
+        TasksPlugin.logInfo("Main config loaded.");
     }
 }
